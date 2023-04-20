2e0ca5e23d3e356c1f111caeac5f0c10fb90c1df
hana-sama <hmama8989@gmail.com>
2023-04-20 09:44:58 +0900

    add files

---
+++ data.py
@@ -0,0 +1,42 @@
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+    {"text": "Approximately one quarter of human bones are in the feet.",
+             "answer": "True"},
+    {"text": "The total surface area of a human lungs is the size of a football pitch.",
+             "answer": "True"},
+    {"text": "In West Virginia, USA, if you accidentally hit an animal with your car,"
+             "you are free to take it home to eat.", "answer": "True"},
+    {"text": "In London, UK, if you happen to die in the House of Parliament,"
+             "you are entitled to a state funeral.", "answer": "False"},
+    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+    {"text": "No piece of square dry paper can be folded in half more than 7 times.",
+             "answer": "False"},
+    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+]
+
+question_data_two = [
+        {"category":"Science: Computers","type":"multiple","difficulty":"easy","question":"According to the International System of Units, how many bytes are in a kilobyte of RAM?","correct_answer":"1000","incorrect_answers":["512","1024","500"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"easy","question":"When Gmail first launched, how much storage did it provide for your email?","correct_answer":"1GB","incorrect_answers":["512MB","5GB","Unlimited"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"medium","question":"Whistler was the codename of this Microsoft Operating System.","correct_answer":"Windows XP","incorrect_answers":["Windows 2000","Windows 7","Windows 95"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"medium","question":"When was the programming language &quot;C#&quot; released?","correct_answer":"2000","incorrect_answers":["1998","1999","2001"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"hard","question":"How many Hz does the video standard PAL support?","correct_answer":"50","incorrect_answers":["59","60","25"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"medium","question":"Which of the following languages is used as a scripting language in the Unity 3D game engine?","correct_answer":"C#","incorrect_answers":["Java","C++","Objective-C"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"medium","question":"The computer OEM manufacturer Clevo, known for its Sager notebook line, is based in which country?","correct_answer":"Taiwan","incorrect_answers":["United States","Germany","China (People&#039;s Republic of)"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"medium","question":"On which day did the World Wide Web go online?","correct_answer":"December 20, 1990","incorrect_answers":["December 17, 1996","November 12, 1990","November 24, 1995"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"easy","question":"What is the domain name for the country Tuvalu?","correct_answer":".tv","incorrect_answers":[".tu",".tt",".tl"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"medium","question":"In the server hosting industry IaaS stands for...","correct_answer":"Infrastructure as a Service","incorrect_answers":["Internet as a Service","Internet and a Server","Infrastructure as a Server"]},
+        {"category":"Science: Computers","type":"boolean","difficulty":"hard","question":"DHCP stands for Dynamic Host Configuration Port.","correct_answer":"False","incorrect_answers":["True"]},
+        {"category":"Science: Computers","type":"boolean","difficulty":"easy","question":"&quot;HTML&quot; stands for Hypertext Markup Language.","correct_answer":"True","incorrect_answers":["False"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"easy","question":"In &quot;Hexadecimal&quot;, what color would be displayed from the color code? &quot;#00FF00&quot;?","correct_answer":"Green","incorrect_answers":["Red","Blue","Yellow"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"hard","question":"Who is the original author of the realtime physics engine called PhysX?","correct_answer":"NovodeX","incorrect_answers":["Ageia","Nvidia","AMD"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"medium","question":"When did the online streaming service &quot;Mixer&quot; launch?","correct_answer":"2016","incorrect_answers":["2013","2009","2011"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"medium","question":"Laserjet and inkjet printers are both examples of what type of printer?","correct_answer":"Non-impact printer","incorrect_answers":["Impact printer","Daisywheel printer","Dot matrix printer"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"medium","question":"What does RAID stand for?","correct_answer":"Redundant Array of Independent Disks","incorrect_answers":["Rapid Access for Indexed Devices","Range of Applications with Identical Designs","Randomized Abstract Identification Description"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"hard","question":"Which of these is not a layer in the OSI model for data communications?","correct_answer":"Connection Layer","incorrect_answers":["Application Layer","Transport Layer","Physical Layer"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"medium","question":"What is the number of keys on a standard Windows Keyboard?","correct_answer":"104","incorrect_answers":["64","94","76"]},
+        {"category":"Science: Computers","type":"multiple","difficulty":"hard","question":"What type of sound chip does the Super Nintendo Entertainment System (SNES) have?","correct_answer":"ADPCM Sampler","incorrect_answers":["FM Synthesizer","Programmable Sound Generator (PSG)","PCM Sampler"]}
+]
