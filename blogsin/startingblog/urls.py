706d9b29093335d6973e8ef23d1e58585d5dc61e
hana-sama <hmama8989@gmail.com>
2024-03-19 07:46:56 +0900

    update app files

--- blogsin/startingblog/urls.py
+++ blogsin/startingblog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView, AddPostView
+from .views import HomeView, ArticleDetailView, AddPostView, BlogUpdateView, BlogDeleteView
 
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
-    path('add_post/', AddPostView.as_view(), name='add_post')
+    path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('<int:pk>/update', BlogUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete', BlogDeleteView.as_view(), name='delete'),
 ]
--- blogsin/startingblog/views.py
+++ blogsin/startingblog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib import messages
 from .models import Post
 from .forms import PostForm
 # Create your views here.
@@ -23,3 +26,34 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
     # fields = '__all__'
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    # form_class = BlogForm
+    login_url = '/home'
+    template_name = 'update_post.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        blog_pk = self.kwargs['pk']
+        url = reverse_lazy('update', kwargs={'pk': blog_pk})
+        return url
+    
+    def form_valid(self, form):
+        messages.success(self.request, "The post has been updated successfully!")
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, "Failed to update the post. Please check the process again!")
+        return super().form_invalid(form)
+        
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_confirm_delete.html'
+    success_url = reverse_lazy('home')
+    login_url = '/home'
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "The post has been deleted!")
+        return super().delete(request, *args, **kwargs)
+
